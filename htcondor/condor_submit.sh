49885338f10b345aeb3761862e80c4f2333c9159
hadib <hadibeydoun8@hotmail.com>
2022-10-23 21:18:25 -0400

    added condor_submit.sh

---
+++ htcondor/condor_submit.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# This script is used to submit a job to the HTCondor batch system.
+# It is called by the condor_submit command.
+# It is not intended to be called directly.
+
+# First Build the Calculating Pi Executable
+cd ../
+cargo build --release
+
+# Stage the Executable
+cp target/release/calculating_pi_rust ./htcondor/calculating_pi_rust
+
+# Reset and Create the Output Directories
+cd htcondor || exit
+rm -rf output error log
+mkdir output error log
+
+# Then Submit the Job
+condor_submit calculating_pi.submit
